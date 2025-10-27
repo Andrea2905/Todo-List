fee516b06714ba3820e15391586a438cb86e314f
Andrea Razo <kimberlyandreary@gmail.com>
2025-10-27 10:04:02 -0600

    agregamos la carpeta use-cases

---
+++ todo-app/src/todos/use-cases/render-todos.js
@@ -0,0 +1,11 @@
+
+import { Todo } from '../../models/todo.model.js';
+
+/**
+ *
+ * @param {String} elementId del componete donde se van a renderizar los todos html
+ * @param {Todo[]} todos arreglo de todos a renderizar
+ */
+export const renderTodos = (elementId, todos = []) => {
+
+};
