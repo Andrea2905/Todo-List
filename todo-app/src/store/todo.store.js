3ee0eb08957e536166ffd7badd6f839bc8c28afc
Andrea Razo <kimberlyandreary@gmail.com>
2025-10-27 09:36:30 -0600

    creo que vamos bien

--- todo-app/src/store/todo.store.js
+++ todo-app/src/store/todo.store.js
@@ -13,7 +13,81 @@ const state = {
         new Todo('Aprender Vite'),
     ],
     filter: Filters.All,
-}
+};
+
 const initStore = () => {
+    console.log(state);
     console.log('Init Store ');
+};
+
+/**
+ * @param {String} description de lo que se va a crear el todo
+ */
+const addTodo = ( description ) => {
+    throw new Error('Function not implemented.');
+};
+
+const loadStorage = () => {
+    throw new Error('Function not implemented.');
+}
+
+/**
+ * @param {string} todoId el identificador del todo que se va a eliminar
+ */
+const deleteTodo = ( todoId ) => {
+    throw new Error('Function not implemented.');
+}
+
+/**
+ * @param {String} todoId : es el identificador que se recibe para cambiar el estado
+ */
+const toggleTodo = ( todoId ) => {
+    throw new Error('Function not implemented.');
+}
+
+const deleteCompleted = () => {
+    throw new Error('Function not implemented.');
+}
+
+/**
+ * @param {String} newFilter el nuevo filtro que se va a aplicar
+ */
+
+const setFilter = (newFilter = Filters.All) => {
+    throw new Error('Function not implemented.');
+};
+
+const getCurrentFilter = () => {
+    throw new Error('Function not implemented.');
+};
+
+
+/**
+ * @param {String} filter el filtro que se va a aplicar, puede ser: all, completed, pending
+ */
+const getTodos = (filter = Filters.All) => {
+    switch(filter) {
+        case Filters.All:
+            return [...state.todos];
+
+        case Filters.Completed:
+            return state.todos.filter(todo => todo.done);
+
+        case Filters.Pending:
+            return state.todos.filter(todo => !todo.done);
+
+        default:
+            throw new Error(`Option ${ filter } is not valid`);
+    }
+};
+
+
+export default {
+    initStore,
+    deleteTodo,
+    getCurrentFilter,
+    deleteCompleted,
+    loadStorage,
+    setFilter,
+    toggleTodo
 }
